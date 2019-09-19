c8907c5e9cb37511c4da7de1564c3f9ae9681f89
alchemistmatt <matthew.monroe@pnnl.gov>
2019-09-19 13:35:55 -0700

    Add example data files

---
+++ Example_Data/RunProgram.bat
@@ -0,0 +1,5 @@
+pushd ..
+
+C:\ProgramData\Anaconda3\python.exe dta_refinery.py Example_Data\Job1610358\DTARef_Lowess_StatCysAlk.xml Example_Data\Job1610358\Adult_Pancreas_Gel_Elite_60_f02_dta.txt c:\DMS_Temp_Org\ID_006331_C5A5E51E.fasta
+
+popd
