dfbefa9a3b5bcefe4db7adc24460d22d526f6b0d
alchemistmatt <matthew.monroe@pnnl.gov>
2019-09-19 16:36:27 -0700

    Add example data

---
+++ Example_Data/MouseVoxel/run_example_numpy_spline.bat
@@ -0,0 +1,5 @@
+pushd ..\..
+
+C:\ProgramData\Anaconda3\python.exe dta_refinery.py Example_Data\MouseVoxel\numpy_spline_settings.xml Example_Data\MouseVoxel\test_dta.txt Example_Data\MouseVoxel\M_musculus_Uniprot_SPROT_2019-09-19.fasta
+
+popd
